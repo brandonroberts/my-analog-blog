6027c66cc5c1eebc1644573319f95e85cd898477
Brandon Roberts <robertsbt@gmail.com>
2023-08-18 09:32:56 -0500

    feat: add markdown renderer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
+import { provideContent, withMarkdownRenderer } from '@analogjs/content';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideFileRouter(),
     provideHttpClient(),
     provideClientHydration(),
+    provideContent(withMarkdownRenderer())
   ],
 };
